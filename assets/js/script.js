d1b5cba0e6501055c5ce00e4233b45309e4def65
SamanthaJoy13 <tavaresjsamantha52@icloud.com>
2023-03-21 18:07:51 -0700

    cleaned up the html & css files,and removed buttons from carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -185,11 +185,19 @@ searchEl.addEventListener("click", function () {
   // .then(data => console.log(data))
   // .catch(error => console.error(error));
 
-
 });
 
 
+// Get the input field
+  //var input = document.getElementById("myInput");
 
-
-
-
+// Execute a function when the user presses a key on the keyboard
+  //input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+    //if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    //event.preventDefault();
+    // Trigger the button element with a click
+    //document.getElementById("myBtn").click();
+  //}
+//});
