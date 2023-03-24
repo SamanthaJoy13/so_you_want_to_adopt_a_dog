d58b1766b51f167d0e5ee48e579f5e9ff61decb0
Rockoban420 <lautaro.avellaneda@gmail.com>
2023-03-23 20:02:28 -0700

    added search history to local storage and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
-
 var searchEl = document.getElementById("searchBtn");
 var textSearchEl = document.getElementById("citySearch");
 var searchResult = document.getElementById("resultsContainer");
 var options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-		'X-RapidAPI-Key': '08f0371562mshaace697a3d212cdp1d709djsn42622047c807',
-		'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
-	}
+    "X-RapidAPI-Key": "08f0371562mshaace697a3d212cdp1d709djsn42622047c807",
+    "X-RapidAPI-Host": "priceline-com-provider.p.rapidapi.com",
+  },
 };
 var hotelList = [{}];
+let searches = JSON.parse(localStorage.getItem("last5Searches")) || {};
+let lastKey;
 
 function displayMap(hotels) {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -18,7 +19,7 @@ function displayMap(hotels) {
   });
 
   for (let i = 1; i < hotels.length; i++) {
-    let position = { lat: hotels[i].lat, lng: hotels[i].long }
+    let position = { lat: hotels[i].lat, lng: hotels[i].long };
     const contentString =
       '<div id="content">' +
       '<div id="siteNotice">' +
@@ -46,18 +47,17 @@ function displayMap(hotels) {
   }
 
   infoWindow = new google.maps.InfoWindow();
- }
+}
 
 function fetchHotels(requestUrl) {
   fetch(requestUrl, options)
     .then(function (response) {
-    //  console.log(response);
+      //  console.log(response);
       return response.json();
     })
     .then(function (data) {
-      console.log (data);
+      console.log(data);
       hotelList = [{}];
-      var counter = 0;
       for (let i = 0; i < data.length; i++) {
         // if (data.sr[i].type == 'CITY' && counter < 1) {
         //   var currentLat = Number(data.sr[i].coordinates.lat);
@@ -69,23 +69,21 @@ function fetchHotels(requestUrl) {
         //   hotelList[0] = Obj;
         //   counter++;
         // }
-          // let oneDayCard = document.createElement("div");
-          // oneDayCard.innerText = data.sr[i].regionNames.shortName;
-          // searchResult.append(oneDayCard);
-          var myObj = {
-            hotelName: data[i].itemName,
-            address: data[i].address,
-            lat: data[i].lat,
-            long: data[i].lon
-          }
-          hotelList.push(myObj);
+        // let oneDayCard = document.createElement("div");
+        // oneDayCard.innerText = data.sr[i].regionNames.shortName;
+        // searchResult.append(oneDayCard);
+        var myObj = {
+          hotelName: data[i].itemName,
+          address: data[i].address,
+          lat: data[i].lat,
+          long: data[i].lon,
+        };
+        hotelList.push(myObj);
       }
-      console.log (hotelList);
+      console.log(hotelList);
       window.initMap = displayMap(hotelList);
-    }
-    )
-  
-  };
+    });
+}
 
 // async function fetchHotels(location) {
 //   const endpoint = 'https://hotels4.p.rapidapi.com/locations/search';
@@ -128,22 +126,47 @@ function fetchHotels(requestUrl) {
 
 // map start
 
-
 //map end
+
+function saveLast5Searches() {
+  // Update the topCityContainer div with the new searches
+  $("#topCityContainer").empty();
+  for (const key in searches) {
+      $("#topCityContainer").append(
+        `<button id="x" class = "btn">${key}</button>`
+      );
+  }
+}
+saveLast5Searches();
+
+$("#topCityContainer").on("click", function (event) {
+  if (event.target.id === "x") {
+    citySearch = event.target.innerText;
+    searches[citySearch] = 1;
+    localStorage.setItem("last5Searches", JSON.stringify(searches));
+    saveLast5Searches();
+    let requestUrl = `https://priceline-com-provider.p.rapidapi.com/v1/hotels/locations?name=${citySearch}&search_type=HOTEL`;
+    fetchHotels(requestUrl);
+  }
+});
 
 searchEl.addEventListener("click", function () {
   citySearch = textSearchEl.value;
-  var myArr = citySearch.split(',');
-  let cityName = myArr[0] +' '+ myArr[1];
-  var requestUrl = `https://priceline-com-provider.p.rapidapi.com/v1/hotels/locations?name=${cityName}&search_type=HOTEL`;
+  let myArr = citySearch.split(",");
+  let cityName = myArr[0] + " " + myArr[1];
+  let cityToUppercase = cityName.toUpperCase();
+  searches[cityToUppercase] = 1;
+  localStorage.setItem("last5Searches", JSON.stringify(searches));
+  saveLast5Searches();
+  let requestUrl = `https://priceline-com-provider.p.rapidapi.com/v1/hotels/locations?name=${cityToUppercase}&search_type=HOTEL`;
   fetchHotels(requestUrl);
   // .then(data => console.log(data))
   // .catch(error => console.error(error));
 });
 
 //Execute a function when the user presses a key on the keyboard
-  textSearchEl.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
+textSearchEl.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
     // Cancel the default action, if needed
     event.preventDefault();
     // Trigger the button element with a click
